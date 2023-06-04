ed0068388d719392ec3dec28e6a238f76a9b642d
Roman <jrj80mail@gmail.com>
2023-06-05 00:41:39 +0300

    4

--- Lesson_2/script.js
+++ Lesson_2/script.js
@@ -44,6 +44,8 @@
 //     В тілі функції є тільки одна інструкція if
 // В тілі функції відсутні інструкції else або else if
 
+//? =============================================
+
 // function checkAge(age) {
 //   if (age >= 18) {
 //     // Change this line
@@ -63,19 +65,109 @@
 // checkAge(38); //  "You are an adult"
 
 //! =============================================
-//* Task 2
+//* Task 2      патерн «раннє повернення» 'return'
 //! =============================================
+// Ф-ція checkPassword отримує пароль користувача у password, перевіряє на збіг з адміністраторот у ADMIN_PASSWORD
+// і повертає повідомлення про результат порівняння.
+// Виконай рефакторинг коду функції checkPassword, використовуючи патерн «раннє повернення»:
+//       видали змінну message
+//       видали else
+//       код повинен працювати так само, як і до оптимізації
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   if (password === ADMIN_PASSWORD) {
+//     message = "Welcome!";
+//   } else {
+//     message = "Access denied, wrong password!";
+//   }
+//   return message;
+// }
+//? =============================================
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   }
+//   return "Access denied, wrong password!";
+// }
+
+// console.log(checkPassword("mangohackzor")); //  "Access denied, wrong password!"
+// console.log(checkPassword("polyhax")); //  "Access denied, wrong password!"
+// console.log(checkPassword("jqueryismyjam")); //  "Welcome!"
+
+// checkPassword("mangohackzor"); //  "Access denied, wrong password!"
+// checkPassword("polyhax"); //  "Access denied, wrong password!"
+// checkPassword("jqueryismyjam"); //  "Welcome!"
 
 //! =============================================
-//* Task 3
+//?            патерн «раннє повернення» 'return'
+//*  Task 3        if () { return "empty!";
+//!                }         !!! else  непотрібно!!!
+//*                if () { return "Your!";
+//*                } return "The";
+//?   console.log("Your order is empty!");       return;
 //! =============================================
 
-//! =============================================
-//* Task 4
-//! =============================================
+//* Ф-ція checkStorage перевіряє можливість оформлення замовлення і повертає повідомлення про результат.
+//* Вона приймає два параметри, значення яких будуть задаватися під час її виклику.
+//*     available - доступна кількість товарів на складі
+//*     ordered - кількість одиниць товару в замовленні
+//* Виконай рефакторинг коду функції checkStorage, використовуючи патерн «раннє повернення».
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   if (ordered === 0) {
+//     message = "Your order is empty!";
+//   } else if (ordered > available) {
+//     message = "Your order is too large, not enough goods in stock!";
+//   } else {
+//     message = "The order is accepted, our manager will contact you";
+//   }
+//   return message;
+// }
+//? =============================================
+
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   }
+//   if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   }
+//   return "The order is accepted, our manager will contact you";
+// }
+
+// checkStorage(100, 50); //  "The order is accepted, our manager will contact you"
+// checkStorage(100, 130); //  "Your order is too large, not enough goods in stock!"
+// checkStorage(70, 0); //  "Your order is empty!"
+// checkStorage(200, 20); //  "The order is accepted, our manager will contact you"
+// checkStorage(200, 250); //  "Your order is too large, not enough goods in stock!"
+// checkStorage(150, 0); //  "Your order is empty!"
 
 //! =============================================
-//* Task 5
+//?              Масив - зберігає впорядковану колекцію елементів.
+//*                  Він оголошується відкритою і закритою квадратною дужкою [] - літералом масиву.
+//* Task 4           Всередині дужок кожен елемент масиву розділяється комою.
+//?                     const planets = ["Earth", "Mars", "Venus"];
+//?                     як правило, використовують const
+//! =============================================
+
+//* Оголоси змінну fruits і присвой їй масив фруктів - рядків "apple", "plum", "pear" і "orange".
+//* Оголошена змінна fruits
+//* Значення змінної fruits - це масив ["apple", "plum", "pear", "orange"]
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+// console.log(fruits);
+
+//! =============================================
+//* Task 5       Для доступу до значення елемента масиву застосовують синтаксис квадратних дужок масив[індекс].
+//*              Між ім'ям змінної масиву і квадратними дужками не повинно бути пробілу.
+//*                     const planets = ["Earth", "Mars", "Venus"];
+//* planets[0]; // "Earth"
+//* planets[2]; // "Venus"
 //! =============================================
 
 //! =============================================
