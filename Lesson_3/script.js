3398060e3265566ceab1be2c4bb9a5455973a61d
Roman <jrj80mail@gmail.com>
2023-06-11 19:29:33 +0300

    16

--- Lesson_3/script.js
+++ Lesson_3/script.js
@@ -664,6 +664,22 @@
 //* Task 16     ВИТРАТИ НА ЗАРПЛАТУ
 //! =============================================
 
+//* ф-ція countTotalSalary(salaries) приймає об'єкт зарплат, де ім'я властивості - це ім'я співробітника, а значення властивості - це зарплата.
+//* повинна розрахувати загальну суму зарплат співробітників (змінна totalSalary) і повернути її.
+//*      змінна totalSalary - загальна сума зарплати.
+//* Ф-ція враховує тільки власні властивості об'єкта
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   const salaryValues = Object.values(salaries);
+//   for (const salary of salaryValues) {
+//     totalSalary += salary;
+//   }
+//   return totalSalary;
+// }
+// console.log(countTotalSalary({})); // 0
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 })); // 330
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 })); // 400
+
 //! =============================================
 //* Task 17
 //! =============================================
