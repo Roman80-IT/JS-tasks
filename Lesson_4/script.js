4317a5f5a273b305dab531940ab6e59cb1d338a7
Roman <jrj80mail@gmail.com>
2023-06-24 00:21:22 +0300

    25

--- Lesson_4/script.js
+++ Lesson_4/script.js
@@ -683,9 +683,11 @@
 
 // const genres = books.flatMap((book) => book.genres);
 // console.log(genres);
+
 //! ============================================================================
 //* Task 17
 //! ============================================================================
+
 //* Доповни функцію getUserNames(users), щоб вона повертала масив імен користувачів (властивість name) з масиву об'єктів в параметрі users.
 //*    Змінній getUserNames присвоєна стрілочна функція з параметром (users).
 //*    Для перебирання параметра users використовується метод map()
@@ -1122,78 +1124,78 @@
 // Змінній getUsersWithAge присвоєна стрілочна функція з параметрами (users, minAge, maxAge)
 // Для перебирання параметра users використовується метод filter()
 
-// const users = [
-//   {
-//     name: "Moore Hensley",
-//     email: "moorehensley@indexia.com",
-//     eyeColor: "blue",
-//     friends: ["Sharron Pace"],
-//     isActive: false,
-//     balance: 2811,
-//     gender: "male",
-//     age: 37,
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     email: "sharlenebush@tubesys.com",
-//     eyeColor: "blue",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     isActive: true,
-//     balance: 3821,
-//     gender: "female",
-//     age: 34,
-//   },
-//   {
-//     name: "Ross Vazquez",
-//     email: "rossvazquez@xinware.com",
-//     eyeColor: "green",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     isActive: false,
-//     balance: 3793,
-//     gender: "male",
-//     age: 24,
-//   },
-//   {
-//     name: "Elma Head",
-//     email: "elmahead@omatom.com",
-//     eyeColor: "green",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     isActive: true,
-//     balance: 2278,
-//     gender: "female",
-//     age: 21,
-//   },
-//   {
-//     name: "Carey Barr",
-//     email: "careybarr@nurali.com",
-//     eyeColor: "blue",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     isActive: true,
-//     balance: 3951,
-//     gender: "male",
-//     age: 27,
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     email: "blackburndotson@furnigeer.com",
-//     eyeColor: "brown",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     isActive: false,
-//     balance: 1498,
-//     gender: "male",
-//     age: 38,
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     email: "shereeanthony@kog.com",
-//     eyeColor: "brown",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     isActive: true,
-//     balance: 2764,
-//     gender: "female",
-//     age: 39,
-//   },
-// ];
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37,
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34,
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24,
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21,
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27,
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38,
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39,
+  },
+];
 
 //! Change code below this line
 //* const getUsersWithAge = (users, minAge, maxAge) => { };
@@ -1208,10 +1210,10 @@
 
 //! ============================================================================
 //?        filter()         includes()         flatMap()
-//* Task 24
+//* Task 24         обєкт - вибір user по елементу який міститься в масиві всередині
 //! ============================================================================
 
-//* Доповни функцію getUsersWithFriend(users, friendName) так, щоб вона повертала масив користувачів, у яких є один з ім'ям в параметрі friendName.
+//* Доповни ф-цію getUsersWithFriend(users, friendName) так, щоб вона повертала масив користувачів, у яких є один з ім'ям в параметрі friendName.
 //*    Масив друзів користувача зберігається у властивості friends.
 //*    Змінній getUsersWithFriend присвоєна стрілочна функція з параметрами (users, friendName)
 //*    Для перебирання параметра users використовується метод filter()
@@ -1243,8 +1245,34 @@
 // console.log(getUsersWithFriend(users, "Adrian Cross")); //     -   []
 
 //! ============================================================================
-//* Task 25
+//?         filter()    indexOf()
+//* Task 25     щоб масив, не містив повторень
 //! ============================================================================
+// students.flatMap((student) => student.courses);
+//* Доповни ф-цію getFriends(users) так, щоб вона повертала масив друзів всіх користувачів (властивість friends).
+//*  У декількох користувачів можуть бути однакові друзі, зроби так, щоб масив, що повертається, не містив повторень.
+//*     Змінній getFriends присвоєна стрілочна функція з параметром (users)
+//*     Виклик функції повертає масив ["Sharron Pace", "Briana Decker", "Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner", "Goldie Gentry", "Aisha Tran", "Jordan Sampson", "Eddie Strong", "Jacklyn Lucas", "Linda Chapman"]
+//! Change code below this line
+//* const getFriends = (users) => {};
+//! Change code above this line
+
+//?        ------- ВАРІАНТ1 --------
+// const getFriends = (users) => {
+//   const allFriends = users.flatMap((user) => user.friends);
+//   const uniqueFriends = allFriends.filter(
+//     (friend, index, arr) => arr.indexOf(friend) === index
+//   );
+
+//   return uniqueFriends;
+// };
+
+//?        ------- ВАРІАНТ2 --------
+// const getFriends = users
+//   .flatMap((user) => user.friends)
+//   .filter((friend, index, arr) => arr.indexOf(friend) === index);
+
+// console.log(getFriends(users));
 
 //! ============================================================================
 //* Task 26
