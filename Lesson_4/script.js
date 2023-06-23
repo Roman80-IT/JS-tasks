271731b9e03c0f8aaee990cd34bf3547fd7cada4
Roman <jrj80mail@gmail.com>
2023-06-23 15:49:57 +0300

    21

--- Lesson_4/script.js
+++ Lesson_4/script.js
@@ -891,7 +891,7 @@
 // console.log(uniqueGenres); //   ["adventure", "history", "fiction", "mysticism", "horror"]
 
 //! ============================================================================
-//?            filter()      { score } (деструктуризація обєктів)     &&
+//?            filter()      { score } (деструктуризація об'єктів)     &&
 //*               Під час роботи з масивом об'єктів виконується фільтрація за значенням певної властивості.
 //*                У підсумку, утворюється новий масив відфільтрованих об'єктів.
 //*                   є масив студентів з балами за тест. Необхідно відфільтрувати кращих (бал вище 80), гірших (бал нижче 50) і середніх студентів (бал від 50 до 80):
@@ -913,48 +913,47 @@
 //                          console.log(average);          //   Масив об'єктів з іменами Poly і Houston
 //! ============================================================================
 
-//* Використовуючи метод filter(), доповни код таким чином, щоб:
+//* Використовуючи метод filter(), доповни код так, щоб:
+//*  У змінній topRatedBooks - масив книг, рейтинг яких (властивість rating) > за або =  MIN_RATING.
+//*  У змінній booksByAuthor - масив книг, написаних автором з ім'ям (властивість author), яке збігається зі AUTHOR.
+//*      Значення змінної books - це масив об'єктів
+//*      Значення змінної MIN_RATING - це число 8
+//*      Значення змінної AUTHOR - це рядок "Bernard Cornwell"
 
-// У змінній topRatedBooks утворився масив книг, рейтинг яких (властивість rating) більший за або дорівнює значенню змінної MIN_RATING.
-// У змінній booksByAuthor утворився масив книг, написаних автором з ім'ям (властивість author), яке збігається зі значенням у змінній AUTHOR.
-// Оголошена змінна books
-// Значення змінної books - це масив об'єктів
-// Оголошена змінна MIN_RATING
-// Значення змінної MIN_RATING - це число 8
-// Оголошена змінна AUTHOR
-// Значення змінної AUTHOR - це рядок "Bernard Cornwell"
-// Оголошена змінна topRatedBooks
-// Значення змінної topRatedBooks - це масив книг з рейтингом, вищим за 8
-// Оголошена змінна booksByAuthor
-// Значення змінної booksByAuthor - це масив книг, автор яких "Bernard Cornwell"
-// Для перебирання масиву books використаний метод filter()
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-];
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+//! Change code below this line
 
-const MIN_RATING = 8;
-const AUTHOR = "Bernard Cornwell";
-// Change code below this line
+//?        ------- ВАРІАНТ1  деструктуризація об'єктів --------
+// const topRatedBooks = books.filter(({ rating }) => rating >= MIN_RATING);
+// const booksByAuthor = books.filter(({ author }) => author === AUTHOR);
 
-const topRatedBooks = books;
-const booksByAuthor = books;
+//?        ------- ВАРІАНТ2 --------
+// const topRatedBooks = books.filter((rate) => rate.rating >= MIN_RATING);
+// const booksByAuthor = books.filter((auth) => auth.author === AUTHOR);
+
+// console.log(topRatedBooks); // - це масив книг з рейтингом, вищим за 8
+// console.log(booksByAuthor); // - це масив книг, автор яких "Bernard Cornwell"
 
 //! ============================================================================
 //* Task 22
