7e699e733f97e70354a5efbe54961ea8170fa632
Roman <jrj80mail@gmail.com>
2023-06-27 10:33:07 +0300

    Update script.js

--- Lesson_5/script.js
+++ Lesson_5/script.js
@@ -1,8 +1,33 @@
 // node Lesson_2/script - запуск через термінал ≡
 
 //! ============================================================================
-//* Task 1
+//?           this
+//* Task 1          Всередині ф-цій можна використовувати зарезервоване ключове слово 'this'
+//*               Під час виконання ф-ції, в this записується посилання на об'єкт, в контексті якого вона була викликана.
+//*                Таким чином, в тілі ф-ції ми можемо отримати доступ до властивостей і методів цього об'єкта.
+// const bookShelf = {
+//   authors: ["Bernard Cornwell", "Robert Sheckley"],
+//   getAuthors() {
+//     return this.authors;
+//   },
+//   addAuthor(authorName) {
+//     this.authors.push(authorName);
+//   },
+// };
+// console.log(bookShelf.getAuthors()); // ["Bernard Cornwell", "Robert Sheckley"]
+// bookShelf.addAuthor("Tanith Lee");
+// console.log(bookShelf.getAuthors()); // ["Bernard Cornwell", "Robert Sheckley", "Tanith Lee"]
+//*                Методи getAuthors і addAuthor - це ф-ції (методи об'єкта), які викликаються в контексті об'єкта bookShelf.
+//*                Під час їх виконання в this записується посилання на об'єкт bookShelf і ми можемо звернутися до його властивостей і методів.
 //! ============================================================================
+
+//* Виконай рефакторинг методів об'єкта pizzaPalace, розставивши відсутні this в місцях звернення до властивостей і методів об'єкта.
+//*    Метод checkPizza об'єкта pizzaPalace використовує this.
+//*    Метод order об'єкта pizzaPalace використовує this
+//*    Виклик pizzaPalace.order("Smoked") повертає рядок "Order accepted, preparing «Smoked» pizza"
+//*    Виклик pizzaPalace.order("Four meats") повертає рядок "Order accepted, preparing «Four meats» pizza"
+//*    Виклик pizzaPalace.order("Big Mike") повертає рядок "Sorry, there is no pizza named «Big Mike»"
+//*    Виклик pizzaPalace.order("Viennese") повертає рядок "Sorry, there is no pizza named «Viennese»"
 
 //! ============================================================================
 //* Task 2
