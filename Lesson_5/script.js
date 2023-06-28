81c30b72ffd472b18fab93b3c4e3f473e1949e90
Roman <jrj80mail@gmail.com>
2023-06-28 18:38:42 +0300

    this

--- Lesson_5/script.js
+++ Lesson_5/script.js
@@ -2,32 +2,25 @@
 
 //! ============================================================================
 //?           this
-//* Task 1          Всередині ф-цій можна використовувати зарезервоване ключове слово 'this'
-//*               Під час виконання ф-ції, в this записується посилання на об'єкт, в контексті якого вона була викликана.
-//*                Таким чином, в тілі ф-ції ми можемо отримати доступ до властивостей і методів цього об'єкта.
-// const bookShelf = {
-//   authors: ["Bernard Cornwell", "Robert Sheckley"],
-//   getAuthors() {
-//     return this.authors;
-//   },
-//   addAuthor(authorName) {
-//     this.authors.push(authorName);
-//   },
-// };
-// console.log(bookShelf.getAuthors()); // ["Bernard Cornwell", "Robert Sheckley"]
-// bookShelf.addAuthor("Tanith Lee");
-// console.log(bookShelf.getAuthors()); // ["Bernard Cornwell", "Robert Sheckley", "Tanith Lee"]
+//*               Всередині ф-цій можна використовувати зарезервоване ключове слово 'this'
+//*                 Під час виконання ф-ції, в this записується посилання на об'єкт, в контексті якого вона була викликана.
+//*                 Отже, в тілі ф-ції ми можемо отримати доступ до властивостей і методів цього об'єкта.
+//                    const bookShelf = {
+//                      authors: ["Bernard Cornwell", "Robert Sheckley"],
+//                      getAuthors() {
+// Task 1                 return this.authors;        },
+//                      addAuthor(authorName) {
+//                        this.authors.push(authorName);           },       };
+//                    console.log(bookShelf.getAuthors()); // ["Bernard Cornwell", "Robert Sheckley"]
+//                    bookShelf.addAuthor("Tanith Lee");
+//                    console.log(bookShelf.getAuthors()); // ["Bernard Cornwell", "Robert Sheckley", "Tanith Lee"]
 //*                Методи getAuthors і addAuthor - це ф-ції (методи об'єкта), які викликаються в контексті об'єкта bookShelf.
 //*                Під час їх виконання в this записується посилання на об'єкт bookShelf і ми можемо звернутися до його властивостей і методів.
 //! ============================================================================
 
-//* Виконай рефакторинг методів об'єкта pizzaPalace, розставивши відсутні this в місцях звернення до властивостей і методів об'єкта.
+//* Виконай рефакторинг методів об'єкта 'pizzaPalace', розставивши відсутні 'this' в місцях звернення до властивостей і методів об'єкта.
 //*    Метод checkPizza об'єкта pizzaPalace використовує this.
 //*    Метод order об'єкта pizzaPalace використовує this
-//*    Виклик pizzaPalace.order("Smoked") повертає рядок "Order accepted, preparing «Smoked» pizza"
-//*    Виклик pizzaPalace.order("Four meats") повертає рядок "Order accepted, preparing «Four meats» pizza"
-//*    Виклик pizzaPalace.order("Big Mike") повертає рядок "Sorry, there is no pizza named «Big Mike»"
-//*    Виклик pizzaPalace.order("Viennese") повертає рядок "Sorry, there is no pizza named «Viennese»"
 
 // const pizzaPalace = {
 //   pizzas: ["Supercheese", "Smoked", "Four meats"],
@@ -46,10 +39,10 @@
 //*   },
 //! Change code above this line
 //   checkPizza(pizzaName) {
-//     return pizzas.includes(pizzaName);
+//     return this.pizzas.includes(pizzaName);
 //   },
 //   order(pizzaName) {
-//     const isPizzaAvailable = checkPizza(pizzaName);
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
 
 //     if (!isPizzaAvailable) {
 //       return `Sorry, there is no pizza named «${pizzaName}»`;
@@ -59,8 +52,13 @@
 //   },
 // };
 
+// console.log(pizzaPalace.order("Smoked")); //    "Order accepted, preparing «Smoked» pizza"
+// console.log(pizzaPalace.order("Four meats")); //    "Order accepted, preparing «Four meats» pizza"
+// console.log(pizzaPalace.order("Big Mike")); //    "Sorry, there is no pizza named «Big Mike»"
+// console.log(pizzaPalace.order("Viennese")); //    "Sorry, there is no pizza named «Viennese»"
+
 //! ============================================================================
-//* Task 2
+//* Task 2      АКАУНТ КОРИСТУВАЧА
 //! ============================================================================
 
 //! ============================================================================
