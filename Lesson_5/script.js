7479f048157555fa34354b22c7ad50784bc5a6eb
Roman <jrj80mail@gmail.com>
2023-07-01 02:17:38 +0300

    Update script.js

--- Lesson_5/script.js
+++ Lesson_5/script.js
@@ -980,7 +980,7 @@ class User {
 class ContentEditor extends User {
   constructor({ email, posts }) {
     //! Виклик конструктора батьківського класу User
-    super(email);
+    // super(email);
     this.posts = posts;
   }
 }
@@ -990,52 +990,51 @@ console.log(editor); // { email: 'mango@mail.com', posts: [] }
 console.log(editor.email); // 'mango@mail.com'
 //! ============================================================================
 
-// Додай класу Admin метод constructor, який приймає один параметр - об'єкт налаштувань з двома властивостями email і accessLevel. Додай класу Admin публічну властивість accessLevel, значення якої буде передаватися під час виклику конструктора.
+//* Додай класу Admin метод constructor, який приймає один параметр - об'єкт налаштувань з двома властивостями email і accessLevel.
+//* Додай класу Admin публічну властивість accessLevel, значення якої буде передаватися під час виклику конструктора.
+//* Щоб показати, як буде використовуватися клас Admin, ми додали ініціалізацію екземпляра під оголошенням класу.
 
-// Щоб показати, як буде використовуватися клас Admin, ми додали ініціалізацію екземпляра під оголошенням класу.
-
-// Оголошений клас Admin
-// Клас Admin наслідує від класу User
-// Клас Admin містить публічну статичну властивість AccessLevel
-// Клас Admin містить метод constructor з параметром у вигляді об'єкта {email, accessLevel}
-// В класі Admin в конструкторі для властивості email використовується звернення до конструктора батьківського класу
+//*   Клас Admin наслідує від класу User
+//*   Клас Admin містить публічну статичну властивість AccessLevel
+//*   Клас Admin містить метод constructor з параметром у вигляді об'єкта {email, accessLevel}
+//* В класі Admin в конструкторі для властивості email використовується звернення до конструктора батьківського класу
 // Звернення до Admin.AccessLevel.BASIC повертає рядок "basic"
 // Звернення до Admin.AccessLevel.SUPERUSER повертає рядок "superuser"
 
-class User {
-  email;
+// class User {
+//   email;
 
-  constructor(email) {
-    this.email = email;
-  }
+//   constructor(email) {
+//     this.email = email;
+//   }
 
-  get email() {
-    return this.email;
-  }
+//   get email() {
+//     return this.email;
+//   }
 
-  set email(newEmail) {
-    this.email = newEmail;
-  }
-}
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
 
-class Admin extends User {
-  //! Change code below this line
+// class Admin extends User {
+//   //! Change code below this line
 
-  static AccessLevel = {
-    BASIC: "basic",
-    SUPERUSER: "superuser",
-  };
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
 
-  //! Change code above this line
-}
+//   //! Change code above this line
+// }
 
-const mango = new Admin({
-  email: "mango@mail.com",
-  accessLevel: Admin.AccessLevel.SUPERUSER,
-});
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
 
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.accessLevel); // "superuser"
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
 
 //! ============================================================================
 //* Task 20
