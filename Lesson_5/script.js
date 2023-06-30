74a3ae2bca9aad8c81699cd275b742ded859d40f
Roman <jrj80mail@gmail.com>
2023-06-30 22:12:16 +0300

    18

--- Lesson_5/script.js
+++ Lesson_5/script.js
@@ -871,15 +871,98 @@
 // console.log(audi.price); // 49000
 
 //! ============================================================================
-//* Task 17
+//?          статичні методи - можуть бути (публічні та приватні)
+//* Task 17            В класі можна оголосити не тільки методи майбутнього екземпляра, а також методи, доступні тільки класу - статичні методи,
+//*                    які можуть бути як публічні, так і приватні. Синтаксис оголошення аналогічний статичним властивостям, за винятком того, що значенням буде метод.
+// class User {
+//   static #takenEmails = [];
+
+//   static isEmailTaken(email) {
+//     return User.#takenEmails.includes(email);
+//   }
+//   #email;
+//   constructor({ email }) {
+//     this.#email = email;
+//     User.#takenEmails.push(email);
+//   }
+// }
+// const mango = new User({ email: "mango@mail.com" });
+// console.log(User.isEmailTaken("poly@mail.com"));
+// console.log(User.isEmailTaken("mango@mail.com"));
+//* Особливість статичних методів полягає у тому, що під час їх виклику ключове слово this посилається на сам клас.
+//* Це означає, що статичний метод може отримати доступ до статичних властивостей класу, але не до властивостей екземпляра.
+//* Логічно, тому що статичні методи викликає сам клас, а не його екземпляри.
 //! ============================================================================
 
-//! ============================================================================
-//* Task 18
-//! ============================================================================
+//* Додай класу Car публічний статичний метод checkPrice (price), що приймає ціну автомобіля. Метод повинен порівняти значення параметра price і приватної статичного властивості MAX_PRICE.
+//*     Якщо ціна автомобіля перевищує максимальну, метод повинен повернути рядок "Error! Price exceeds the maximum".
+//*     В іншому випадку метод повинен повернути рядок "Success! Price is within acceptable limits".
+//* Під оголошенням класу ми додали ініціалізацію екземпляра і виклики методів, щоб показати, як буде використовуватися метод checkPrice(price).
+
+//* Клас Car містить статичний метод checkPrice(price)
+//* Виклик Car.checkPrice(36000) повертає рядок "Success! Price is within acceptable limits"
+//* Виклик Car.checkPrice(18000) повертає рядок "Success! Price is within acceptable limits"
+//* Виклик Car.checkPrice(64000) повертає рядок "Error! Price exceeds the maximum"
+//* Виклик Car.checkPrice(57000) повертає рядок "Error! Price exceeds the maximum"
+
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   //! Change code below this line
+//   static checkPrice(price) {
+//?     if (price > Car.#MAX_PRICE) {
+//       return "Error! Price exceeds the maximum";
+//     } else {
+//       return "Success! Price is within acceptable limits";
+//     }
+//   }
+
+//   //! Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
 
 //! ============================================================================
-//* Task 19
+//?         extends
+//? Task 18         class Child extends Parent
+//! ============================================================================
+
+//* У застосунку потрібен адміністратор з можливістю додавати пошти користувачів у чорний список.
+//* Оголоси клас Admin, який наслідує від класу User
+//* Додай класу Admin публічну статичну властивість AccessLevel (рівень доступу), значення якої — це об'єкт {BASIC: "basic", SUPERUSER: "superuser"}
+//* Оголошений клас Admin
+//* Клас Admin наслідує від класу User
+//* Клас Admin містить публічну статичну властивість AccessLevel
+
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// //! Change code below this line
+// class Admin extends User {
+//   static AccessLevel = { BASIC: "basic", SUPERUSER: "superuser" };
+// }
+
+// console.log(Admin.AccessLevel.BASIC); // рядок "basic"
+// console.log(Admin.AccessLevel.SUPERUSER); // рядок "superuser"
+
+//! ============================================================================
+//? Task 19           super(аргументи)
 //! ============================================================================
 
 //! ============================================================================
